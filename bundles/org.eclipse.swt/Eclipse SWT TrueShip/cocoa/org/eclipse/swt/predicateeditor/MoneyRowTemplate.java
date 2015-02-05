52509d7fad9938321a2be9101cc74c09a9838175
Climent Luchianov <climent.luchianov@gmail.com>
2015-02-05 15:54:51 +0200

    Add support for money through the MoneyRowTemplate

---
+++ bundles/org.eclipse.swt/Eclipse SWT TrueShip/cocoa/org/eclipse/swt/predicateeditor/MoneyRowTemplate.java
@@ -0,0 +1,11 @@
+package org.eclipse.swt.predicateeditor;
+
+import org.eclipse.swt.predicateeditor.cocoa.*;
+
+public class MoneyRowTemplate {
+    SWTMoneyRowTemplate swtTemplate;
+
+    public MoneyRowTemplate(SWTMoneyRowTemplate template) {
+        swtTemplate = template;
+    }
+}
