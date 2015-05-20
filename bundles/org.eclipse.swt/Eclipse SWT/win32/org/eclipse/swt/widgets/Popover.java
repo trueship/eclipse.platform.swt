c8436457b185c647df4663f7a305e65e0086f530
Igor Karpenko <karpenko_iv@hotmail.com>
2015-05-20 13:27:40 +0300

    Fix with Popover : Win stub correction

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Popover.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Popover.java
@@ -30,7 +30,7 @@ public class Popover extends Composite {
         MaxYEdge  // Specifies the top edge of the input rectangle.
     }
 
-    public Popover (Popover parent, Composite target, int style) {
+    public Popover (Composite parent, Composite target, int style) {
         super(parent, style);
     }
 
